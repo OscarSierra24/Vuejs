e01bab4e79f71cc64819bb64e5f6c1aab83ce0a8
OscarSierra24 <oscar_sierra_02@hotmail.com>
2019-06-03 14:38:12 -0500

    added logs

--- the-monster-slayer/app.js
+++ the-monster-slayer/app.js
@@ -3,7 +3,8 @@ new Vue({
   data: {
     playerHealth: 100,
     monsterHealth: 100,
-    gameIsRunning: false
+    gameIsRunning: false,
+    turns: []
   },
   watch: {
 
@@ -16,9 +17,16 @@ new Vue({
       this.gameIsRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+      this.turns = [];
     },
     attack: function() {
-      this.monsterHealth -= this.calculateDamage(3, 10);
+      var damage = this.calculateDamage(3, 10);
+      this.monsterHealth -= damage;
+      
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hits Moster for ' + damage
+      });
 
       if(this.checkWin()) {
         return;
@@ -27,7 +35,14 @@ new Vue({
       this.monsterAttacks();
     },
     specialAttack: function() {
-      this.monsterHealth -= this.calculateDamage(10,20);
+      var damage = this.calculateDamage(10,20)
+      this.monsterHealth -= damage;
+
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hits Moster HARD for ' + damage
+      });
+
       if (this.checkWin()) {
         return;
       }
@@ -40,14 +55,25 @@ new Vue({
         this.playerHealth = 100;
       }
 
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player heals for 10'
+      });
+
       this.monsterAttacks();
     },
     giveUp: function() {
       this.gameIsRunning = false;
     },
     monsterAttacks: function() {
-      this.playerHealth -= this.calculateDamage(5, 12);
+      var damage = this.calculateDamage(5, 12);
+      this.playerHealth -= damage;
       this.checkWin();
+
+      this.turns.unshift({
+        isPlayer: false,
+        text: 'Monster hits Player for ' + damage
+      });
     },
     calculateDamage: function(min, max) {
       return Math.max(Math.floor(Math.random() * max) + 1, min);
