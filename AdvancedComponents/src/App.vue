b0511c40b5b5dbec9008bde2d6286bafe68e0739
OscarSierra24 <oscar_sierra_02@hotmail.com>
2019-06-05 18:11:17 -0500

    practicing with slots

---
+++ AdvancedComponents/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <app-quote>
+                    <h2 slot="title">The Quote</h2>
+                    <p slot="content">A wonderful quote!</p>
+                </app-quote>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Quote from './components/Quote.vue';
+    export default {
+        components: {
+            appQuote: Quote
+        }
+    }
+</script>
+
+<style>
+</style>
